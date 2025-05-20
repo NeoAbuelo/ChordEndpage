055b9a87f4dc07cb5b6055ff5c97a0e4c3a29a3a
Neoabu <neo.rikr2@gmail.com>
2025-05-20 12:49:56 -0400

    frist commint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
+import { dark } from '@clerk/themes'
+import { esES } from '@clerk/localizations'
+
+import tailwindcss from '@tailwindcss/vite';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [clerk({
+    localization: esES,
+    appearance: {
+      baseTheme: dark,
+    }}), react()],
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  adapter: node({ mode: "standalone" }),
+  output: "server",
+});
